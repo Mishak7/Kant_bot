8a0accd673816c45974a07815b1af5b2508f0775
Maria <118299224+grupunzel@users.noreply.github.com>
2025-08-29 15:00:30 +0300

    Update language_check_keyboard.py

--- handlers/language_check_handlers/language_check_keyboard.py
+++ handlers/language_check_handlers/language_check_keyboard.py
@@ -4,12 +4,14 @@ Keyboard to choose from language exercises
 
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from handlers.main_handlers.languages import TEXTS
+from handlers.main_handlers.commands import get_user_language
+
+language = get_user_language(callback.from_user.id)
 
 def language_keyboard() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text=f"\U0001F508 {TEXTS['ru']['keyboards']['language_check_keyboard']['language_check_keyboard']['audio']}", callback_data='language_audio')],
-        [InlineKeyboardButton(text=f"\U0001F4DA {TEXTS['ru']['keyboards']['language_check_keyboard']['language_check_keyboard']['grammar']}", callback_data='language_grammar')],
-        [InlineKeyboardButton(text=f"\U0001F4AC {TEXTS['ru']['keyboards']['language_check_keyboard']['language_check_keyboard']['speaking']}", callback_data='language_speaking')],
-        [InlineKeyboardButton(text=f"◀️ {TEXTS['ru']['keyboards']['language_check_keyboard']['language_check_keyboard']['back']}", callback_data='back_to_main')],
+        [InlineKeyboardButton(text=f"\U0001F508 {TEXTS[language]['keyboards']['language_check_keyboard']['language_check_keyboard']['audio']}", callback_data='language_audio')],
+        [InlineKeyboardButton(text=f"\U0001F4DA {TEXTS[language]['keyboards']['language_check_keyboard']['language_check_keyboard']['grammar']}", callback_data='language_grammar')],
+        [InlineKeyboardButton(text=f"\U0001F4AC {TEXTS[language]['keyboards']['language_check_keyboard']['language_check_keyboard']['speaking']}", callback_data='language_speaking')],
+        [InlineKeyboardButton(text=f"◀️ {TEXTS[language]['keyboards']['language_check_keyboard']['language_check_keyboard']['back']}", callback_data='back_to_main')],
     ])
-
