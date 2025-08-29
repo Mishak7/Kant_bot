375b0701375ed58b370bf028044d01748fb51643
Maria <118299224+grupunzel@users.noreply.github.com>
2025-08-29 14:56:11 +0300

    Update critical_keyboard.py

--- handlers/critical_info_handlers/critical_keyboard.py
+++ handlers/critical_info_handlers/critical_keyboard.py
@@ -10,29 +10,22 @@ Keyboards for handlers of critical info.
 
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from handlers.main_handlers.languages import TEXTS
+from handlers.main_handlers.commands import get_user_language
+
+language = get_user_language(callback.from_user.id)
 
 def critical_keyboard() -> InlineKeyboardMarkup:
     """Creates a keyboard layout for emergency contacts."""
     return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text=f"\U0001F46E {TEXTS['ru']['keyboards']['critical_keyboard']['police']}", callback_data='critical_police')],
-        [InlineKeyboardButton(text=f"\U0001F4DE {TEXTS['ru']['keyboards']['critical_keyboard']['hotline']}", callback_data='critical_hotline')],
-        [InlineKeyboardButton(text=f"\U0001F3EC {TEXTS['ru']['keyboards']['critical_keyboard']['government']}", callback_data='critical_government')],
-        [InlineKeyboardButton(text=f"\U0001F3E2 {TEXTS['ru']['keyboards']['critical_keyboard']['consulate']}", callback_data='critical_consulate')],
-        [InlineKeyboardButton(text=f"◀️ {TEXTS['ru']['keyboards']['critical_keyboard']['back']}", callback_data='back_to_main')]
+        [InlineKeyboardButton(text=f"\U0001F46E {TEXTS[language]['keyboards']['critical_keyboard']['police']}", callback_data='critical_police')],
+        [InlineKeyboardButton(text=f"\U0001F4DE {TEXTS[language]['keyboards']['critical_keyboard']['hotline']}", callback_data='critical_hotline')],
+        [InlineKeyboardButton(text=f"\U0001F3EC {TEXTS[language]['keyboards']['critical_keyboard']['government']}", callback_data='critical_government')],
+        [InlineKeyboardButton(text=f"\U0001F3E2 {TEXTS[language]['keyboards']['critical_keyboard']['consulate']}", callback_data='critical_consulate')],
+        [InlineKeyboardButton(text=f"◀️ {TEXTS[language]['keyboards']['critical_keyboard']['back']}", callback_data='back_to_main')]
     ])
 
 def back_to_critical_keyboard():
     """Returns a single-button keyboard to go back to the 'Critical' section."""
     return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text=f"◀️ {TEXTS['ru']['keyboards']['critical_keyboard']['back']}", callback_data="critical")]
-    ])
-
-def info_keyboard() -> InlineKeyboardMarkup:
-    """Generates an information menu keyboard for university-related details."""
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="📅 Расписание", callback_data="schedule")],
-        [InlineKeyboardButton(text="💰 Стипендии", callback_data="scholarship")],
-        [InlineKeyboardButton(text="📞 Контакты учебного офиса", callback_data="office_contacts")],
-        [InlineKeyboardButton(text="🌍 Визово-миграционный центр", callback_data="visa_center")],
-        [InlineKeyboardButton(text="◀️ Назад", callback_data="back_to_main")]
+        [InlineKeyboardButton(text=f"◀️ {TEXTS[language]['keyboards']['critical_keyboard']['back']}", callback_data="critical")]
     ])
