fc1a1e452bef002407ac5b38d587c4dfebfccf8e
Maria <118299224+grupunzel@users.noreply.github.com>
2025-08-29 15:01:53 +0300

    Update keyboard.py

--- handlers/main_handlers/keyboard.py
+++ handlers/main_handlers/keyboard.py
@@ -7,23 +7,46 @@ aiogram's InlineKeyboardMarkup and InlineKeyboardButton.
 """
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from handlers.main_handlers.languages import TEXTS
+from handlers.main_handlers.commands import get_user_language
+
+language = get_user_language(callback.from_user.id)
 
 def main_roots_keyboard() -> InlineKeyboardMarkup:
-    """Create the main menu keyboard with all available options."""
+    """Creates the main menu keyboard with all available options."""
     return InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text=f"🎓 {TEXTS['ru']['keyboards']['main_keyboard']['info']}", callback_data="info"),
-            InlineKeyboardButton(text=f"📍 {TEXTS['ru']['keyboards']['main_keyboard']['location']}", callback_data="location")],
-            [InlineKeyboardButton(text=f"🏘️ {TEXTS['ru']['keyboards']['main_keyboard']['dormitory']}", callback_data="dormitory"),
-            InlineKeyboardButton(text=f"🏥 {TEXTS['ru']['keyboards']['main_keyboard']['hospital']}", callback_data="hospital")],
-            [InlineKeyboardButton(text=f"⚠️ {TEXTS['ru']['keyboards']['main_keyboard']['critical']}", callback_data="critical"),
-            InlineKeyboardButton(text=f"🇷🇺 {TEXTS['ru']['keyboards']['main_keyboard']['language_check']}", callback_data="language_check")],
-            [InlineKeyboardButton(text=f"💳 {TEXTS['ru']['keyboards']['main_keyboard']['sber']}", callback_data="sber")]
+            [InlineKeyboardButton(text=f"🎓 {TEXTS[language]['keyboards']['main_keyboard']['info']}", callback_data="info")],
+            [InlineKeyboardButton(text=f"📍 {TEXTS[language]['keyboards']['main_keyboard']['location']}", callback_data="location")],
+            [InlineKeyboardButton(text=f"🏘️ {TEXTS[language]['keyboards']['main_keyboard']['dormitory']}", callback_data="dormitory")],
+            [InlineKeyboardButton(text=f"🏥 {TEXTS[language]['keyboards']['main_keyboard']['hospital']}", callback_data="hospital")],
+            [InlineKeyboardButton(text=f"⚠️ {TEXTS[language]['keyboards']['main_keyboard']['critical']}", callback_data="critical")],
+            [InlineKeyboardButton(text=f"🇷🇺 {TEXTS[language]['keyboards']['main_keyboard']['language_check']}", callback_data="language_check")]
+        ]
+    )
+
+def lenguage_selection() -> InlineKeyboardMarkup:
+    """Creates a language selection keyboard at the start"""
+    return InlineKeyboardButton(
+        inline_keyboard=[
+            [InlineKeyboardButton(text='\U0001F1F7 Русский', callback_data='russian')],
+            [InlineKeyboardButton(text='\U0001F1EC English', callback_data='english')],
+            [InlineKeyboardButton(text='\U0001F1EB Français', callback_data='french')],
+            [InlineKeyboardButton(text='\U0001F1EA Español', callback_data='spanish')],
+            [InlineKeyboardButton(text='\U0001F1E8 中國的', callback_data='chinese')],
+            [InlineKeyboardButton(text='\U0001F1EE हिंदी', callback_data='indian')]
+        ]
+    )
+
+def back_to_language_selection() -> InlineKeyboardMarkup:
+    """Creates a back button keyboard for language selection"""
+    return InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text=f'◀️ {TEXTS[language]['keyboards']['language_selection_keyboard']['back']}', callback_data='back_to_language_selection')]
         ]
     )
 
 def back_to_main_keyboard() -> InlineKeyboardMarkup:
-    """Create a simple back button keyboard for returning to main menu."""
+    """Creates a simple back button keyboard for returning to main menu."""
     return InlineKeyboardMarkup(inline_keyboard=[
-                                [InlineKeyboardButton(text=f"◀️ {TEXTS['ru']['keyboards']['main_keyboard']['back']}", callback_data="back_to_main")]]
+                                [InlineKeyboardButton(text=f"◀️ {TEXTS[language]['keyboards']['main_keyboard']['back']}", callback_data="back_to_main")]]
     )
