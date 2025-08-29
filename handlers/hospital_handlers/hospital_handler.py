02d561399d55c92f169d9cfb8c56d128c2925f7a
Maria <118299224+grupunzel@users.noreply.github.com>
2025-08-29 14:57:18 +0300

    Update hospital_handler.py

--- handlers/hospital_handlers/hospital_handler.py
+++ handlers/hospital_handlers/hospital_handler.py
@@ -5,23 +5,24 @@ Handler for hospital button
 from aiogram import Router, F
 from aiogram.types import CallbackQuery
 from handlers.main_handlers.keyboard import back_to_main_keyboard
-from handlers.hospital_handlers.hospital_keyboard import hospital_keyboard
 from config.logger import logger
 import traceback
 from handlers.main_handlers.languages import TEXTS
+from handlers.main_handlers.commands import get_user_language
 
 router = Router()
+language = get_user_language(callback.from_user.id)
 
 @router.callback_query(F.data == "hospital")
 async def hospital_handler(callback: CallbackQuery):
     try:
         await callback.message.edit_text(HOSPITAL_TEXT,
                                      parse_mode="Markdown",
-                                     reply_markup=hospital_keyboard())
+                                     reply_markup=back_to_main_keyboard())
         await callback.answer()
     except Exception as e:
         logger.error(f'Hospital info error: {e}\n{traceback.format_exc()}')
-        await callback.answer(f"{TEXTS['ru']['errors']['info_error']}")
+        await callback.answer(f"{TEXTS[language]['errors']['info_error']}")
 
 
-HOSPITAL_TEXT = TEXTS['ru']['handlers']['hospital_handlers']['hospital_text']
+HOSPITAL_TEXT = TEXTS[language]['handlers']['hospital_handlers']['hospital_text']
