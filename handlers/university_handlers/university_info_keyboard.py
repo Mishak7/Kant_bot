863ff115d5f43e64bd6c26338011ab3b8a0f65dd
Maria <118299224+grupunzel@users.noreply.github.com>
2025-08-29 15:03:23 +0300

    Update university_info_keyboard.py

--- handlers/university_handlers/university_info_keyboard.py
+++ handlers/university_handlers/university_info_keyboard.py
@@ -3,43 +3,22 @@ Keyboards for university info
 """
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from handlers.main_handlers.languages import TEXTS
+from handlers.main_handlers.commands import get_user_language
+
+language = get_user_language(callback.from_user.id)
 
 def info_keyboard() -> InlineKeyboardMarkup:
     """Generates an information menu keyboard for university-related details."""
     return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text=f"📅 {TEXTS['ru']['keyboards']['university_info_keyboard']['schedule']}", callback_data="schedule")],
-        [InlineKeyboardButton(text=f"💰 {TEXTS['ru']['keyboards']['university_info_keyboard']['scholarship']}", callback_data="scholarship")],
-        [InlineKeyboardButton(text=f"📞 {TEXTS['ru']['keyboards']['university_info_keyboard']['office_contacts']}", callback_data="office_contacts")],
-        [InlineKeyboardButton(text=f"🌍 {TEXTS['ru']['keyboards']['university_info_keyboard']['visa_center']}", callback_data="visa_center")],
-        [InlineKeyboardButton(text=f"◀️ {TEXTS['ru']['keyboards']['university_info_keyboard']['back']}", callback_data="back_to_main")]
+        [InlineKeyboardButton(text=f"📅 {TEXTS[language]['keyboards']['university_info_keyboard']['schedule']}", callback_data="schedule")],
+        [InlineKeyboardButton(text=f"💰 {TEXTS[language]['keyboards']['university_info_keyboard']['scholarship']}", callback_data="scholarship")],
+        [InlineKeyboardButton(text=f"📞 {TEXTS[language]['keyboards']['university_info_keyboard']['office_contacts']}", callback_data="office_contacts")],
+        [InlineKeyboardButton(text=f"🌍 {TEXTS[language]['keyboards']['university_info_keyboard']['visa_center']}", callback_data="visa_center")],
+        [InlineKeyboardButton(text=f"◀️ {TEXTS[language]['keyboards']['university_info_keyboard']['back']}", callback_data="back_to_main")]
     ])
 
 def back_to_info_keyboard() -> InlineKeyboardMarkup:
     """Go back to uni info keyboard"""
     return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text=f"◀️ {TEXTS['ru']['keyboards']['university_info_keyboard']['back']}", callback_data='info')]
+        [InlineKeyboardButton(text=f"◀️ {TEXTS[language]['keyboards']['university_info_keyboard']['back']}", callback_data='info')]
     ])
-
-def schedule_info_keyboard() -> InlineKeyboardMarkup:
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text= "📅 Расписание" , url="https://schedule.kantiana.ru/")],
-        [InlineKeyboardButton(text=f"◀️ {TEXTS['ru']['keyboards']['university_info_keyboard']['back']}",
-                              callback_data='info')]
-    ])
-
-def scholarship_info_keyboard() -> InlineKeyboardMarkup:
-    """Go back to uni info keyboard"""
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="💰Стипендия", url="https://kantiana.ru/students/scholarship/")],
-        [InlineKeyboardButton(text=f"◀️ {TEXTS['ru']['keyboards']['university_info_keyboard']['back']}", callback_data='info')]
-    ])
-
-def visa_info_keyboard() -> InlineKeyboardMarkup:
-    """Go back to uni info keyboard"""
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="🌍Визово-миграционный центр", url="https://kantiana.ru/universitys/administration/mezhdunarodnyy-ofis/")],
-        [InlineKeyboardButton(text=f"◀️ {TEXTS['ru']['keyboards']['university_info_keyboard']['back']}", callback_data='info')]
-    ])
-
-
-
