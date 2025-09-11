637b41e91f7b0bbcdda4ceff54d6f8edbf0c29ff
grupunzel <maria.yu.nikitina@icloud.com>
2025-09-11 20:50:48 +0300

    fixing

--- handlers/critical_info_handlers/critical_keyboard.py
+++ handlers/critical_info_handlers/critical_keyboard.py
@@ -10,11 +10,8 @@ Keyboards for handlers of critical info.
 
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from handlers.main_handlers.languages import TEXTS
-from handlers.main_handlers.commands import get_user_language
 
-language = get_user_language(callback.from_user.id)
-
-def critical_keyboard() -> InlineKeyboardMarkup:
+def critical_keyboard(language: str) -> InlineKeyboardMarkup:
     """Creates a keyboard layout for emergency contacts."""
     return InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text=f"\U0001F46E {TEXTS[language]['keyboards']['critical_keyboard']['police']}", callback_data='critical_police')],
@@ -24,7 +21,7 @@ def critical_keyboard() -> InlineKeyboardMarkup:
         [InlineKeyboardButton(text=f"◀️ {TEXTS[language]['keyboards']['critical_keyboard']['back']}", callback_data='back_to_main')]
     ])
 
-def back_to_critical_keyboard():
+def back_to_critical_keyboard(language: str):
     """Returns a single-button keyboard to go back to the 'Critical' section."""
     return InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text=f"◀️ {TEXTS[language]['keyboards']['critical_keyboard']['back']}", callback_data="critical")]
